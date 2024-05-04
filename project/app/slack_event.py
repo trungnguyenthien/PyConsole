0c1445a64e78b84d17ab3e190dcdcb5ab7f1c0ef
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-05-04 14:03:14 +0700

    .

--- project/app/slack_event.py
+++ project/app/slack_event.py
@@ -1,24 +1,17 @@
 # views.py
 from django.http import HttpResponse
-from slack_sdk.signature import SignatureVerifier
-from slack_sdk.errors import SlackRequestError
 from .log import log
 from django.http import JsonResponse
 import json
 
-# Thay thế bằng Signing Secret của ứng dụng Slack
-SLACK_SIGNING_SECRET = "3135f508999027a39f18c8c0d3a297a0"
-# xoxb-356896431271-7054590596066-G0Q4LHfxB1oBXKFxHTFaWmrs
-
-# Tạo đối tượng SignatureVerifier
-signature_verifier = SignatureVerifier(SLACK_SIGNING_SECRET)
-
 def slack_events(request):
-  # Lấy dữ liệu từ yêu cầu Slack
+    # Lấy dữ liệu từ yêu cầu Slack và giải mã từ UTF-8
     body = request.body.decode("utf-8")
     headers = dict(request.headers)
-    log(body)
-    log(headers)
+
+    # Log headers và body với format dễ đọc
+    log(json.dumps(headers, indent=2))  # JSON format cho headers
+    log(json.dumps(json.loads(body), indent=2))  # JSON format cho body
 
     json_data = json.loads(body)
     
