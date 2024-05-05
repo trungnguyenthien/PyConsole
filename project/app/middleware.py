b8f2b74de9912f0f2b6725f45f4e75da268c0fc2
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-05-06 06:47:12 +0700

    .

--- project/app/middleware.py
+++ project/app/middleware.py
@@ -3,6 +3,9 @@ from .utils.log import log
 class RequestLoggerMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+      
+    def process_exception(self, request, exception):
+      log({'request': request, 'exception': exception })
 
     def __call__(self, request):
         # Ghi log cho request
