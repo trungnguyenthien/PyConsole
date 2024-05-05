332e7a3fc6ac79cd447e5c8e1b7a419802bf8e8b
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-05-05 18:41:07 +0700

    .

--- project/app/service/chatgpt.py
+++ project/app/service/chatgpt.py
@@ -18,17 +18,20 @@ async def request_text(system_specs, assistant_specs, user_message):
         messages.append({"role": "assistant", "content": content})
     messages.append({"role": "user", "content": user_message})
     logger.log(f'message = {messages}')
-
-    response = await client.chat.completions.create(
-            model="gpt-4",  # Sử dụng mô hình GPT-4
-            messages=messages,
-            # temperature=0.7,  # Điều chỉnh độ đa dạng của câu trả lời (0-2)
-            # max_tokens=10000,  # Giới hạn số lượng token trong câu trả lời
-            # top_p=1,
-            # frequency_penalty=0,
-            # presence_penalty=0,
-            # request_timeout=60,  # Thời gian chờ tối đa (giây)
-        )
+    try:
+      response = await client.chat.completions.create(
+              model="gpt-4",  # Sử dụng mô hình GPT-4
+              messages=messages,
+              # temperature=0.7,  # Điều chỉnh độ đa dạng của câu trả lời (0-2)
+              # max_tokens=10000,  # Giới hạn số lượng token trong câu trả lời
+              # top_p=1,
+              # frequency_penalty=0,
+              # presence_penalty=0,
+              # request_timeout=60,  # Thời gian chờ tối đa (giây)
+          )
+    except Exception as e:
+        print(f"An error occurred while generating response: {e}")
+    
     logger.log(f"response-----------\n{response}")
     reply = response.choices[0].message["content"]
     logger.log(f"Response from OPENAI-------------\n{reply}")
