962181434de4f0ceca4d376ea6071955c1466a4d
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-05-05 17:42:51 +0700

    .

--- project/app/utils/secret.py
+++ project/app/utils/secret.py
@@ -1,5 +1,5 @@
 import os
 
-openApi_key = os.environ.get("OPENAI_API_KEY")
-slack_bot_token = os.environ.get("SLACK_BOT_OAUTH_TOKEN")
-slack_user_token = os.environ.get("SLACK_USER_OAUTH_TOKEN")
+openApi_key = os.getenv("OPENAI_API_KEY")
+slack_bot_token = os.getenv("SLACK_BOT_OAUTH_TOKEN")
+slack_user_token = os.getenv("SLACK_USER_OAUTH_TOKEN")
