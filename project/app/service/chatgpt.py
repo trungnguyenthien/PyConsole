02d26b9a6d71508787c23ef0c88045e0c7cf7611
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-05-05 19:31:46 +0700

    .

--- project/app/service/chatgpt.py
+++ project/app/service/chatgpt.py
@@ -24,6 +24,7 @@ def request_text(system_specs, assistant_specs, user_message):
             presence_penalty=0
         )
         logger.log('3')
+        logger.log(f'response = {response}')
         reply = response.choices[0].message["content"]
         logger.log('4')
         return reply
