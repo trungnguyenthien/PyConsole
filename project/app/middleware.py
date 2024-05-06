e79ab80bd7a7f33edbef947e0da892c9d1d8f437
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-05-06 12:28:36 +0700

    .

--- project/app/middleware.py
+++ project/app/middleware.py
@@ -1,11 +1,12 @@
 from .utils.log import log
+import json
 
 class RequestLoggerMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
       
     def process_exception(self, request, exception):
-      log({'request': request, 'exception': exception })
+      log(json.dumps({'request': request, 'exception': exception }))
 
     def __call__(self, request):
         # Ghi log cho request
