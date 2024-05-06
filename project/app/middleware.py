0a31139a8e120e7e9ae635e2e3b61b805601cb34
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-05-07 05:49:49 +0700

    .

--- project/app/middleware.py
+++ project/app/middleware.py
@@ -2,12 +2,9 @@ from .utils.log import log
 import json
 from django.utils.deprecation import MiddlewareMixin
 
-class RequestLoggerMiddleware(MiddlewareMixin):
+class RequestLoggerMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-      
-    def process_exception(self, request, exception):
-      log(json.dumps({'request': request, 'exception': exception }))
 
     def __call__(self, request):
         # Ghi log cho request
