575cdc1df20a4a53a88d91671a53fb6f9f2e0149
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-05-14 05:27:58 +0700

    .

--- project/app/service/slack.py
+++ project/app/service/slack.py
@@ -17,7 +17,7 @@ def update_message(channel_id, ts, new_message):
             ts=ts,
             text=new_message
         )
-        log(f'chat_update response = {json.dumps(response)}')
+        log(f'chat_update response = {vars(response)}')
         log(["Message updated successfully:", response['message']['text']])
     except SlackApiError as e:
         log(f"Error updating message: {e.response['error']}")
@@ -31,7 +31,7 @@ def send_new_message(channel_id, message):
             channel=channel_id,
             text=message
         )
-        log(f'send_new_message response = {json.dumps(response)}')
+        log(f'send_new_message response = {vars(response)}')
         log(["Message sent successfully:", response['message']['text']])
     except SlackApiError as e:
         log(f"Error sending message: {e.response['error']}")
