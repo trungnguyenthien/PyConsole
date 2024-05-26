277a3542709f3217c25c61fcd43c9c95aa3d031e
HaiPham <pthai076@icloud.com>
2024-05-26 18:31:42 +0700

    feat: get all conversion in thread

--- project/app/service/slack.py
+++ project/app/service/slack.py
@@ -74,3 +74,20 @@ def delete_message(channel_id, message_id):
         return response
     except SlackApiError as e:
         log(f"Error delete message: {e.response['error']}")
+
+
+def all_conversions(channel_id, message_id):
+    # Cần phải add con bot vào channel cần lấy message
+    # Example usage:
+    # all_conversions('C1234567890', '1714993823.193669')
+    print(f'all_conversions({channel_id}, {message_id})')
+    try:
+        # Call the chat.chatDelete method using the built-in WebClient
+        response = client.conversations_replies(
+            channel=channel_id,
+            ts=message_id
+        )
+        print(["Load message successfully:", response])
+        return response
+    except SlackApiError as e:
+        print(f"Error sending message: {e.response['error']}")
