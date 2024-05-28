ea86b95ded4f8f3e35b2c827428c0add23b3d75b
trungnguyenthien <ngthientrung@gmail.com>
2024-05-29 05:57:19 +0700

    .

--- project/app/manager/slack_cmd.py
+++ project/app/manager/slack_cmd.py
@@ -18,20 +18,33 @@ summary_command_list = ["@bot_magic", "@comter", "@MyComter"]
 
 
 def slack_summary(request):
-    # Lấy dữ liệu từ yêu cầu Slack và giải mã từ UTF-8
-    # try:
-    #    log("EVENT POSTBODY\n" + json.dumps(request.body, indent=2))
-    # except Exception as e:
-    #     log(f"Error updating message: {e.response['error']}")
-
     body = request.body.decode("utf-8")
+    body_dict = dict(item.split("=") for item in body.split("&"))
     headers = dict(request.headers)
     log("EVENT HEADER\n" + json.dumps(headers, indent=2))
-    log("EVENT POSTBODY\n" + body)
-    json_data = json.loads(body)
-
-    # Kiểm tra loại sự kiện
-    if 'type' in json_data and json_data['type'] == 'url_verification':
-        # Trả lại challenge code mà Slack gửi
-        return JsonResponse({'challenge': json_data['challenge']})
+    log("EVENT POSTBODY\n" + json.dumps(body_dict, indent=2))
+    
+    text = body_dict['text']
+    
     return response_to_slack_received_event
+
+'''
+SAMPLE body_dict
+
+{
+  'token': 'OifjAOHsLCZZ3u8wvqUF4o7s',
+  'team_id': 'TAGSCCP7Z',
+  'team_domain': 'ntrung',
+  'channel_id': 'C071P11UWHJ',
+  'channel_name': 'jp_channel',
+  'user_id': 'UAF8W1P33',
+  'user_name': 'ngthientrung',
+  'command': '%2Fsummary',
+  'text': 'llll',
+  'api_app_id': 'A071NP30LCU',
+  'is_enterprise_install': 'false',
+  'response_url': 'https%3A%2F%2Fhooks.slack.com%2Fcommands%2FTAGSCCP7Z%2F7187489248834%2F3nnoeuKaCRtzNIdwwhxSeAdR',
+  'trigger_id': '7200157835345.356896431271.03a872a87805c05d3cc17baeb74e1d74'
+}
+
+'''
