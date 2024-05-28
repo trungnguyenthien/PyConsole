2ce17a4e47f70de9e045dad834566125474cd1ec
trungnguyenthien <ngthientrung@gmail.com>
2024-05-29 06:11:17 +0700

    .

--- project/app/manager/slack_cmd.py
+++ project/app/manager/slack_cmd.py
@@ -33,7 +33,7 @@ def slack_summary(request):
     print(f"channel_id = '{channel_id}'")
     print(f"ts = '{ts}'")
     
-    return JsonResponse(status=200)
+    return JsonResponse({"response_type": "in_channel"}, status=200)
 
 '''
 SAMPLE body_dict
